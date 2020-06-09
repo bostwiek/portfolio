0173507d6886f443342b7066ef4a80cfb3b11a94
bostwiek <bostwiek@users.noreply.github.com>
2020-06-08 21:21:03 -0600

    Start to build out skeleton based on .psd design mocks

--- src/App.js
+++ src/App.js
@@ -10,91 +10,82 @@ function App() {
     <div className="App">
 			
 			<header>
-				<div className="logo"><strong>Brendon</strong>Mancini</div>
-				<nav className="nav-header">
-					<ul>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Skills</a></li>
-						<li><a href="#">Work</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
+				<nav className="nav-desktop">
+					<a href="#">About</a>
+					<a href="#">Skills</a>
+					<a href="#">Projects</a>
+					<a href="#">Contact</a>
 				</nav>
-				<div className="clear"></div>
+				<nav className="nav-mobile"></nav>
 			</header>
 
 			<div className="container about-container">
-				<div className="container-left">
-					<h3>Designer</h3>
+				<div className="about-col">
+					<div className="about-logo"></div>
+					<span>Hi, I'm <strong>Brendon</strong></span>
 				</div>
-				<div className="container-right">
-					<h3>Developer</h3>
+				<div className="about-col">
+					<span>a <strong>Front End Developer</strong> by trade,</span>
+				</div>
+				<div className="about-col">
+					<span>and an <strong>illustrator</strong>, <strong>animator</strong>, and <strong>designer</strong> by passion.</span>
 				</div>
 			</div>
 
 
 
 			<div className="container skills-container">
-				<h3>Skills</h3>
-				<div className="container-left">
-					<ul>
-						<li>Photoshop</li>
-						<li>Photoshop</li>
-						<li>Photoshop</li>
-						<li>Photoshop</li>
-						<li>Photoshop</li>
-					</ul>
+				<h1>So, what can I do?</h1>
+				<div className="skills-col">
+					<p>
+						I have 4 years experience working as a Front End and an Email/Marketing Developer for Eat24, Ytelp, and Grubhub.<br />
+						Outside of work, I’ve spent the last few years adding to an illustration portfolio,  studying ui/ux design, and wrote, voiced, and animated some dark comedy cartoons.
+					</p>
 				</div>
-				<div className="container-right">
-					<ul>
+				<div className="skills-col">
+					<div className="col-left">
+						<h3><strong>Front End Development</strong></h3>
+						<ul className="skills-list">
+						<li>Javascript (ES6)</li>
+						<li>Node</li>
 						<li>React</li>
-						<li>React</li>
-						<li>React</li>
-						<li>React</li>
-						<li>React</li>
-					</ul>
+						<li>PHP</li>
+						<li>HTML/CSS</li>
+						<li>Email Development</li>
+						<li>Git</li>
+						<li>JIRA</li>
+
+						</ul>
+					</div>
+					<div className="col-right">
+						<h3><strong>Design</strong></h3>
+						<ul className="skills-list">
+							<li>Photoshop</li>
+							<li>Illustrator</li>
+							<li>After Effects</li>
+							<li>Unity</li>
+							<li>Figma</li>
+							<li>Procreate</li>
+
+						</ul>
+					</div>
 				</div>
+
 			</div>
 
 			<div className="container project-container">
 				<h3>Projects</h3>
-				<div className="project-card">
-					<div className="container-left">
-						<div className="project-thumbnail">
-							<img src={thumbnail} width="200" />
-						</div>
-					</div>
-					<div className="container-right">
-						<div className="project-title">
-							Project Title
-						</div>
-						<div className="project-description">
-							Project description project description project description project description project description project description project description project description project description project description project description project description project description
-						</div>
-					</div>
-				</div>
-
-				<div className="project-card">
-					<div className="container-left">
-						<div className="project-thumbnail">
-							<img src={thumbnail} width="200" />
-						</div>
-					</div>
-					<div className="container-right">
-						<div className="project-title">
-							Project Title
-						</div>
-						<div className="project-description">
-							Project description project description project description project description project description project description project description project description project description project description project description project description project description
-						</div>
-					</div>
-				</div>
-				
+				<div className="project-card"></div>
+				<div className="project-card"></div>
+				<div className="project-card"></div>
 			</div>
 
 			<div className="container contact-container">
 				<h3>Contact</h3>
 				<input type="text" maxLength="100" />
+				<br />
 				<textarea maxLength="2000" />
+				<br />
 				<button>Submit</button>
 			</div>
 
